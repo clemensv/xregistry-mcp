1f334f5c0fe2012ae8dad43b11df773a067acadc
Clemens Vasters <clemens@vasters.com>
2025-04-24 22:14:28 +0200

    script update
    Signed-off-by: Clemens Vasters <clemens@vasters.com>

--- xreg/run-registry.sh
+++ xreg/run-registry.sh
@@ -7,6 +7,11 @@ cd xregistry-server
 make cmds
 cd ../
 
+cd ..
+
 docker run -ti -p 8080:8080 ghcr.io/xregistry/xrserver-all --recreatedb
 sleep 30
 xregistry-server/xr model update xreg/model.json -s localhost:8080
+
+xregistry-server/xr create mcpproviders/google/servers/maps -d @registry/mcpproviders/google/servers/maps/index.json -s localhost:8080
+xregistry-server/xr download -s localhost:8080 live -u https://clemensv.github.io/xregistry-mcp/
